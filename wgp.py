f8ead119f82bc1da37abee1f1d7969ec28c07d72
samukbg <samukbg@users.noreply.github.com>
2026-08-03 18:12:40 +0300

    fix: disable expandable_segments on Windows to prevent CUDA resource already mapped error

--- wgp.py
+++ wgp.py
@@ -1,8 +1,7 @@
 import os, sys, traceback
 os.environ["GRADIO_LANG"] = "en"
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "garbage_collection_threshold:0.5,expandable_segments:True"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "garbage_collection_threshold:0.8"
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning, message=".*expandable_segments.*")
 
 
 def safe_cuda_cleanup():
